da1af439f9e2f1bc7a896d4f2005d8186ef9cc95
Михаил <mikhail.markin@rtlabs.ru>
2022-09-26 12:32:34 +0300

    [EDT] api liveness sound

--- api_test_liveness_sound.sh
+++ api_test_liveness_sound.sh
@@ -3,7 +3,7 @@
 #                        #
 # Author: kflirik        #
 #                        #
-# version: 1.24.0        #
+# version: 1.24.1        #
 ##########################
 
 # include functions
@@ -11,21 +11,21 @@ source include/f_checks.sh
 
 BODY="tmp/responce_body"
 EMPTY="resources/samples/empty"
-META="resources/metadata/meta_sound.json"
+META="resources/metadata/meta_lv_sound_passive.json"
+META_WM="resources/metadata/meta_lv_s_p_without_mnemonic.json"
+META_WA="resources/metadata/meta_lv_s_p_without_action.json"
+META_WT="resources/metadata/meta_lv_s_p_without_type.json"
+META_WD="resources/metadata/meta_lv_s_p_without_duration.json"
+META_WMSG="resources/metadata/meta_lv_s_p_without_message.json"
+
 SAMPLE_WAV="resources/samples/sound.wav"
-SAMPLE_PCM="resources/samples/sound.raw"
+SAMPLE_PCM="resources/samples/sound.pcm"
+SAMPLE_NV="resources/samples/sound_without_voice.wav"
+SAMPLE_DV="resources/samples/sound_double_voice.wav"
 
 SAMPLE_JPG="resources/samples/photo_shumskiy.jpg"
-SAMPLE_PNG="resources/samples/photo.png"
 SAMPLE_WEBM="resources/samples/video.mov"
-SAMPLE_NF="resources/samples/no_face.jpg"
-SAMPLE_TF="resources/samples/two_face.jpg"
 SAMPLE_CAT_JPG="resources/samples/cat.jpg"
-META_WM="resources/metadata/meta_without_mnemonic.json"
-META_WA="resources/metadata/meta_without_action.json"
-META_WT="resources/metadata/meta_without_type.json"
-META_WD="resources/metadata/meta_without_duration.json"
-META_WMSG="resources/metadata/meta_without_message.json"
 
 f_test_liveness() {
     VENDOR_URL="$BASE_URL/detect"
@@ -34,7 +34,7 @@ f_test_liveness() {
     REQUEST='curl -s -w "%{http_code}" -H "Content-Type:multipart/form-data" -F "metadata=@'$META';type=application/json" -F "bio_sample=@'$SAMPLE_WAV';type=audio/wav" --output '$BODY' '$VENDOR_URL
     f_check -r 200 -m "[0-1].[0-9]" -f "- Score format double is expected"
     
-    TEST_NAME="Positive test 2. detect sound.wav"
+    TEST_NAME="Positive test 2. detect sound.pcm"
     REQUEST='curl -s -w "%{http_code}" -H "Content-Type:multipart/form-data" -F "metadata=@'$META';type=application/json" -F "bio_sample=@'$SAMPLE_PCM';type=audio/pcm" --output '$BODY' '$VENDOR_URL
     f_check -r 200 -m "[0-1].[0-9]" -f "- Score format double is expected"
 
@@ -49,72 +49,72 @@ f_test_liveness() {
     REQUEST='curl --max-time 15000 -s -w "%{http_code}" -H "Expect:" -H "Content-type:multipart/form-data; boundary=72468" --data-binary @tmp/request_body --output '$BODY' '$VENDOR_URL
     f_check -r 200 -m "[0-1].[0-9]" -f "- Score format double is expected"
 
-    TEST_NAME="Negative test 0. Cat photo"
-    REQUEST='curl -s -w "%{http_code}" -H "Content-Type:multipart/form-data" -F "metadata=@'$META';type=application/json" -F "bio_sample=@'$SAMPLE_CAT_JPG';type=image/jpeg" --output '$BODY' '$VENDOR_URL
-    f_check -r 400 -m "LDE-003002"
+#    TEST_NAME="Negative test 0. Cat photo"
+#    REQUEST='curl -s -w "%{http_code}" -H "Content-Type:multipart/form-data" -F "metadata=@'$META';type=application/json" -F "bio_sample=@'$SAMPLE_CAT_JPG';type=image/jpeg" --output '$BODY' '$VENDOR_URL
+#    f_check -r 400 -m "LDE-003002"
 
     TEST_NAME="Negative test 1. Incorrect Content-Type"
-    REQUEST='curl -s -w "%{http_code}" -H "Content-Type:application/json" -F "metadata=@'$META';type=application/json" -F "bio_sample=@'$SAMPLE_JPG';type=image/jpeg" --output '$BODY' '$VENDOR_URL
+    REQUEST='curl -s -w "%{http_code}" -H "Content-Type:application/json" -F "metadata=@'$META';type=application/json" -F "bio_sample=@'$SAMPLE_WAV';type=audio/wav" --output '$BODY' '$VENDOR_URL
     f_check -r 400 -m "LDE-002001"
 
     TEST_NAME="Negative test 2. Request with incorrect HTTP method"
-    REQUEST='curl -s -w "%{http_code}" -H "Content-Type:multipart/form-data" -F "metadata=@'$META';type=application/json" -F "bio_sample=@'$SAMPLE_JPG';type=image/jpg" -X GET --output '$BODY' '$VENDOR_URL
+    REQUEST='curl -s -w "%{http_code}" -H "Content-Type:multipart/form-data" -F "metadata=@'$META';type=application/json" -F "bio_sample=@'$SAMPLE_WAV';type=audio/wav" -X GET --output '$BODY' '$VENDOR_URL
     f_check -r 400 -m "LDE-002002"
 
     TEST_NAME="Negative test 3. Request with empty bio_sample"
-    REQUEST='curl -s -w "%{http_code}" -H "Content-Type:multipart/form-data" -F "metadata=@'$META';type=application/json" -F "bio_sample=@'$EMPTY';type=image/jpeg" --output '$BODY' '$VENDOR_URL
+    REQUEST='curl -s -w "%{http_code}" -H "Content-Type:multipart/form-data" -F "metadata=@'$META';type=application/json" -F "bio_sample=@'$EMPTY';type=audio/wav" --output '$BODY' '$VENDOR_URL
     f_check -r 400 -m "LDE-002004"
 
     TEST_NAME="Negative test 4. Incorrect Content-Type part of multipart"
-    REQUEST='curl -s -w "%{http_code}" -H "Content-Type:multipart/form-data" -F "metadata=@'$META';type=image/jpeg" -F "bio_sample=@'$SAMPLE_JPG';type=image/jpeg" --output '$BODY' '$VENDOR_URL
+    REQUEST='curl -s -w "%{http_code}" -H "Content-Type:multipart/form-data" -F "metadata=@'$META';type=image/jpeg" -F "bio_sample=@'$SAMPLE_WAV';type=audio/wav" --output '$BODY' '$VENDOR_URL
     f_check -r 400 -m "LDE-002005"
 
     TEST_NAME="Negative test 5. Incorrect Content-Type part of multipart"
+    REQUEST='curl -s -w "%{http_code}" -H "Content-Type:multipart/form-data" -F "metadata=@'$META';type=application/json" -F "bio_sample=@'$SAMPLE_WAV';type=application/json" --output '$BODY' '$VENDOR_URL
+    f_check -r 400 -m "LDE-002005"
+
+    TEST_NAME="Negative test 6. Request with photo"
+    REQUEST='curl -s -w "%{http_code}" -H "Content-Type:multipart/form-data" -F "metadata=@'$META';type=application/json" -F "bio_sample=@'$SAMPLE_JPG';type=image/jpeg" --output '$BODY' '$VENDOR_URL
+    f_check -r 400 -m "LDE-002005"
+
+    TEST_NAME="Negative test 7. Request with photo"
     REQUEST='curl -s -w "%{http_code}" -H "Content-Type:multipart/form-data" -F "metadata=@'$META';type=application/json" -F "bio_sample=@'$SAMPLE_JPG';type=application/json" --output '$BODY' '$VENDOR_URL
-    f_check -r 400 -m "LDE-002005"
-
-    TEST_NAME="Negative test 6. Request with sound"
-    REQUEST='curl -s -w "%{http_code}" -H "Content-Type:multipart/form-data" -F "metadata=@'$META';type=application/json" -F "bio_sample=@'$SAMPLE_WAV';type=audio/wav" --output '$BODY' '$VENDOR_URL
-    f_check -r 400 -m "LDE-002005"
-
-    TEST_NAME="Negative test 7. Request with sound"
-    REQUEST='curl -s -w "%{http_code}" -H "Expect:" -H "Content-Type:multipart/form-data" -F "metadata=@'$META';type=application/json" -F "bio_sample=@'$SAMPLE_WAV';type=image/jpeg" --output '$BODY' '$VENDOR_URL
     f_check -r 400 -m "LDE-002004"
 
     TEST_NAME="Negative test 8. Request with video"
-    REQUEST='curl -s -w "%{http_code}" -H "Expect:" -H "Content-Type:multipart/form-data" -F "metadata=@'$META';type=application/json" -F "bio_sample=@'$SAMPLE_WEBM';type=image/jpeg" --output '$BODY' '$VENDOR_URL
+    REQUEST='curl -s -w "%{http_code}" -H "Content-Type:multipart/form-data" -F "metadata=@'$META';type=application/json" -F "bio_sample=@'$SAMPLE_WEBM';type=audio/wav" --output '$BODY' '$VENDOR_URL
     f_check -r 400 -m "LDE-002004"
 
     TEST_NAME="Negative test 9. Request with video"
     REQUEST='curl -s -w "%{http_code}" -H "Content-Type:multipart/form-data" -F "metadata=@'$META';type=application/json" -F "bio_sample=@'$SAMPLE_WEBM';type=video/webm" --output '$BODY' '$VENDOR_URL
     f_check -r 400 -m "LDE-002005"
 
-    TEST_NAME="Negative test 10. Request with no face"
-    REQUEST='curl -s -w "%{http_code}" -H "Content-Type:multipart/form-data" -F "metadata=@'$META';type=application/json" -F "bio_sample=@'$SAMPLE_NF';type=image/jpeg" --output '$BODY' '$VENDOR_URL
+    TEST_NAME="Negative test 10. Request with no voice"
+    REQUEST='curl -s -w "%{http_code}" -H "Content-Type:multipart/form-data" -F "metadata=@'$META';type=application/json" -F "bio_sample=@'$SAMPLE_NV';type=audio/wav" --output '$BODY' '$VENDOR_URL
     f_check -r 400 -m "LDE-003002"
 
-    TEST_NAME="Negative test 11. Request with two face"
-    REQUEST='curl -s -w "%{http_code}" -H "Content-Type:multipart/form-data" -F "metadata=@'$META';type=application/json" -F "bio_sample=@'$SAMPLE_TF';type=image/jpeg" --output '$BODY' '$VENDOR_URL
+    TEST_NAME="Negative test 11. Request with two voice"
+    REQUEST='curl -s -w "%{http_code}" -H "Content-Type:multipart/form-data" -F "metadata=@'$META';type=application/json" -F "bio_sample=@'$SAMPLE_DV';type=audio/wav" --output '$BODY' '$VENDOR_URL
     f_check -r 400 -m "LDE-003003"
 
     TEST_NAME="Negative test 12. Request with meta without mnemonic"
-    REQUEST='curl -s -w "%{http_code}" -H "Expect:" -H "Content-Type:multipart/form-data" -F "metadata=@'$META_WM';type=application/json" -F "bio_sample=@'$SAMPLE_JPG';type=image/jpeg" --output '$BODY' '$VENDOR_URL
+    REQUEST='curl -s -w "%{http_code}" -H "Expect:" -H "Content-Type:multipart/form-data" -F "metadata=@'$META_WM';type=application/json" -F "bio_sample=@'$SAMPLE_WAV';type=audio/wav" --output '$BODY' '$VENDOR_URL
     f_check -r 400 -m "LDE-003004"
 
     TEST_NAME="Negative test 13. Request with meta without action"
-    REQUEST='curl -s -w "%{http_code}" -H "Expect:" -H "Content-Type:multipart/form-data" -F "metadata=@'$META_WA';type=application/json" -F "bio_sample=@'$SAMPLE_JPG';type=image/jpeg" --output '$BODY' '$VENDOR_URL
+    REQUEST='curl -s -w "%{http_code}" -H "Expect:" -H "Content-Type:multipart/form-data" -F "metadata=@'$META_WA';type=application/json" -F "bio_sample=@'$SAMPLE_WAV';type=audio/wav" --output '$BODY' '$VENDOR_URL
     f_check -r 400 -m "LDE-003004"
 
     TEST_NAME="Negative test 14. Request with meta without action.type"
-    REQUEST='curl -s -w "%{http_code}" -H "Expect:" -H "Content-Type:multipart/form-data" -F "metadata=@'$META_WT';type=application/json" -F "bio_sample=@'$SAMPLE_JPG';type=image/jpeg" --output '$BODY' '$VENDOR_URL
+    REQUEST='curl -s -w "%{http_code}" -H "Expect:" -H "Content-Type:multipart/form-data" -F "metadata=@'$META_WT';type=application/json" -F "bio_sample=@'$SAMPLE_WAV';type=audio/wav" --output '$BODY' '$VENDOR_URL
     f_check -r 400 -m "LDE-003004"
 
     TEST_NAME="Negative test 15. Request with meta without action.duration"
-    REQUEST='curl -s -w "%{http_code}" -H "Expect:" -H "Content-Type:multipart/form-data" -F "metadata=@'$META_WD';type=application/json" -F "bio_sample=@'$SAMPLE_JPG';type=image/jpeg" --output '$BODY' '$VENDOR_URL
+    REQUEST='curl -s -w "%{http_code}" -H "Expect:" -H "Content-Type:multipart/form-data" -F "metadata=@'$META_WD';type=application/json" -F "bio_sample=@'$SAMPLE_WAV';type=audio/wav" --output '$BODY' '$VENDOR_URL
     f_check -r 400 -m "LDE-003004"
 
     TEST_NAME="Negative test 16. Request with meta without action.message"
-    REQUEST='curl -s -w "%{http_code}" -H "Expect:" -H "Content-Type:multipart/form-data" -F "metadata=@'$META_WMSG';type=application/json" -F "bio_sample=@'$SAMPLE_JPG';type=image/jpeg" --output '$BODY' '$VENDOR_URL
+    REQUEST='curl -s -w "%{http_code}" -H "Expect:" -H "Content-Type:multipart/form-data" -F "metadata=@'$META_WMSG';type=application/json" -F "bio_sample=@'$SAMPLE_WAV';type=audio/wav" --output '$BODY' '$VENDOR_URL
     f_check -r 400 -m "LDE-003004"
 }
 
